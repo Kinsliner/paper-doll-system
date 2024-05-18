94e14c86402d83e2255ce805eba0c396ee3c31ca
echen <echen.wan9@gmail.com>
2024-05-18 16:09:08 +0800

    [更新]分離程式碼

--- paper-doll-system/Assets/PaperDoll/Scripts/Data/EzDataLoader.cs
+++ paper-doll-system/Assets/PaperDoll/Scripts/Data/EzDataLoader.cs
@@ -2,37 +2,6 @@ using Ez;
 using Ez.EzEditor;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
-
-public class StreamingAssetPath : IPath
-{
-    public string DataName { get; set; }
-
-    public StreamingAssetPath()
-    {
-    }
-
-    public StreamingAssetPath(string dataName)
-    {
-        DataName = dataName;
-    }
-
-    public string GetName<T>()
-    {
-        if (string.IsNullOrEmpty(DataName) == false)
-        {
-            return DataName;
-        }
-
-        return typeof(T).Name;
-    }
-
-    public string GetPath()
-    {
-        return Path.Combine(Application.streamingAssetsPath, "GameData");
-    }
-}
 
 public class EzDataLoader
 {
---
+++ paper-doll-system/Assets/PaperDoll/Scripts/Data/StreamingAssetPath.cs
@@ -0,0 +1,32 @@
+﻿using Ez;
+using System.IO;
+using UnityEngine;
+
+public class StreamingAssetPath : IPath
+{
+    public string DataName { get; set; }
+
+    public StreamingAssetPath()
+    {
+    }
+
+    public StreamingAssetPath(string dataName)
+    {
+        DataName = dataName;
+    }
+
+    public string GetName<T>()
+    {
+        if (string.IsNullOrEmpty(DataName) == false)
+        {
+            return DataName;
+        }
+
+        return typeof(T).Name;
+    }
+
+    public string GetPath()
+    {
+        return Path.Combine(Application.streamingAssetsPath, "GameData");
+    }
+}
