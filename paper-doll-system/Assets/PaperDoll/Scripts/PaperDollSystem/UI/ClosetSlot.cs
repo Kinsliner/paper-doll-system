5c58e4a9b0f74432900fe6a0154ed265bfe77b3c
echen <echen.wan9@gmail.com>
2024-05-19 15:54:04 +0800

    [新增]衣櫃欄位UI

---
+++ paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/ClosetSlot.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosetSlot : MonoBehaviour
+{
+    public bool IsEmpty { get; private set; }
+
+    public void Init()
+    {
+    }
+
+    public void Clear()
+    {
+    }
+
+    public void Display(PaperDollController.PaperDollCache paperDollCache)
+    {
+
+    }
+}
