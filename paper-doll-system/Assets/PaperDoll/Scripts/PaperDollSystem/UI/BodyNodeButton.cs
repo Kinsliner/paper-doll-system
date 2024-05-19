e65af33f675010f7762509db83c0ba01942f64d7
echen <echen.wan9@gmail.com>
2024-05-19 18:18:51 +0800

    [更新]部位按鈕

--- paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/BodyNodeButton.cs
+++ paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/BodyNodeButton.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using Ez.Tool;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BodyNodeButton : MonoBehaviour
 {
+    [System.Serializable]
+    private struct BodyNodeIcon
+    {
+        public BodyNode node;
+        public Sprite image;
+    }
+
     public Action<BodyNode> OnClickEvent;
+
+    [SerializeField]
+    private List<BodyNodeIcon> iconList = new List<BodyNodeIcon>();
+
+    [SerializeField]
+    private UICollector uiCollector;
 
     private BodyNode bodyNode;
 
     public void Init(BodyNode bodyNode)
     {
         this.bodyNode = bodyNode;
+
+        SetIcon(bodyNode);
+    }
+
+    private void SetIcon(BodyNode bodyNode)
+    {
+        foreach (var icon in iconList)
+        {
+            if (icon.node == bodyNode)
+            {
+                // Set icon
+                uiCollector.SetImage(UIKey.BodyNodeButton_IconImage, icon.image);
+                break;
+            }
+        }
     }
 
     public void OnClick()
