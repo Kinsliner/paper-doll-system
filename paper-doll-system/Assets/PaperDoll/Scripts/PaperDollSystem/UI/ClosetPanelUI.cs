fa85661b42ae0e6326cbaf9444b73334ee4cd082
echen <echen.wan9@gmail.com>
2024-05-19 15:54:42 +0800

    [更新]衣櫃功能UI

--- paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/ClosetPanelUI.cs
+++ paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/ClosetPanelUI.cs
@@ -1,5 +1,8 @@
 using Ez.Tool;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ClosetPanelUI : MonoBehaviour
@@ -7,10 +10,212 @@ public class ClosetPanelUI : MonoBehaviour
     [SerializeField]
     private UICollector uiCollector;
 
-    private PaperDollController paperDollController;
+    [SerializeField]
+    private GameObject bodyNodeButtonPrefab;
 
+    [SerializeField]
+    private GameObject closetSlotPrefab;
+
+    [SerializeField]
+    private int slotPerPage = 10;
+
+    private PaperDollController paperDollController;
+    private List<BodyNodeButton> bodyNodeButtons = new List<BodyNodeButton>();
+    private List<ClosetSlot> closetSlots = new List<ClosetSlot>();
+    private List<PaperDollController.PaperDollCache> currentCaches = new List<PaperDollController.PaperDollCache>();
+    private BodyNode currentNode;
+    private int currentPage = 0;
+    private int maxPage = 0;
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
     public void Init()
     {
-        paperDollController = PaperDollManager.Controller; ;
+        paperDollController = PaperDollManager.Controller;
+
+        BuildBodyNodeButtons();
+        BuildClosetSlots();
+        SetupPageButtons();
+    }
+
+    private void BuildBodyNodeButtons()
+    {
+        var root = uiCollector.GetRectTransform(UIKey.Closet_BodyNodeButtonRoot);
+
+        var options = Enum.GetValues(typeof(BodyNode));
+
+        // 清除舊的按鈕
+        foreach (BodyNodeButton button in bodyNodeButtons)
+        {
+            button.OnClickEvent -= OnBodyNodeButtonClicked;
+            Destroy(button.gameObject);
+        }
+        bodyNodeButtons.Clear();
+
+        // 建立新的按鈕
+        foreach (BodyNode option in options)
+        {
+            var button = Instantiate(bodyNodeButtonPrefab, root);
+            var bodyNodeButton = button.GetComponent<BodyNodeButton>();
+            bodyNodeButton.Init(option);
+            bodyNodeButton.OnClickEvent += OnBodyNodeButtonClicked;
+            bodyNodeButtons.Add(bodyNodeButton);
+        }
+    }
+
+    private void OnBodyNodeButtonClicked(BodyNode node)
+    {
+        RefreshCloset(node);
+    }
+
+    private void BuildClosetSlots()
+    {
+        var root = uiCollector.GetRectTransform(UIKey.Closet_SlotRoot);
+
+        // 清除舊的 Slot
+        foreach (ClosetSlot slot in closetSlots)
+        {
+            Destroy(slot.gameObject);
+        }
+        closetSlots.Clear();
+
+        // 建立新的 Slot
+        for (int i = 0; i < slotPerPage; i++)
+        {
+            var slot = Instantiate(closetSlotPrefab, root);
+            var closetSlot = slot.GetComponent<ClosetSlot>();
+            closetSlot.Init();
+            closetSlots.Add(closetSlot);
+        }
+    }
+
+    private void SetupPageButtons()
+    {
+        uiCollector.BindOnClick(UIKey.Closet_PrevButton, OnPreviousButtonClicked);
+        uiCollector.BindOnClick(UIKey.Closet_NextButton, OnNextButtonClicked);
+    }
+
+    private void OnNextButtonClicked()
+    {
+        ToPage(currentPage + 1);
+    }
+
+    private void OnPreviousButtonClicked()
+    {
+        ToPage(currentPage - 1);
+    }
+
+    /// <summary>
+    /// 切換到指定頁數
+    /// </summary>
+    /// <param name="page">頁數</param>
+    public void ToPage(int page)
+    {
+        currentPage = Mathf.Clamp(page, 1, maxPage);
+        uiCollector.SetText(UIKey.Closet_PageText, $"{currentPage}/{maxPage}");
+        DisplayBodyItems(currentPage);
+    }
+
+    /// <summary>
+    /// 重新整理目前部位的衣櫃，並顯示在第一頁
+    /// </summary>
+    public void RefreshCloset()
+    {
+        // 取得部位的所有項目
+        currentCaches = paperDollController.GetPaperDollCaches(currentNode);
+
+        // 重置頁數
+        ResetPages();
+
+        // 顯示項目在Slot上
+        DisplayBodyItems(currentPage);
+    }
+
+    /// <summary>
+    /// 重新整理指定部位的衣櫃，並顯示在第一頁
+    /// </summary>
+    /// <param name="node"></param>
+    public void RefreshCloset(BodyNode node)
+    {
+        // 設定目前部位
+        currentNode = node;
+
+        // 取得部位的所有項目
+        currentCaches = paperDollController.GetPaperDollCaches(currentNode);
+
+        // 重置頁數
+        ResetPages();
+
+        // 顯示項目在Slot上
+        DisplayBodyItems(currentPage);
+    }
+
+    /// <summary>
+    /// 重新整理指定部位的衣櫃，並指定要跳到的頁數
+    /// </summary>
+    public void RefeshCloset(BodyNode node, int page)
+    {
+        // 設定目前部位
+        currentNode = node;
+
+        // 取得部位的所有項目
+        currentCaches = paperDollController.GetPaperDollCaches(currentNode);
+
+        // 重置頁數，並跳到指定頁數
+        ResetToPage(page);
+
+        // 顯示項目在Slot上
+        DisplayBodyItems(currentPage);
+    }
+
+    /// <summary>
+    /// 重置頁面資料
+    /// </summary>
+    private void ResetPages()
+    {
+        // 計算總頁數
+        maxPage = Mathf.CeilToInt((float)currentCaches.Count / slotPerPage);
+        currentPage = 1;
+
+        // 顯示頁數
+        uiCollector.SetText(UIKey.Closet_PageText, $"{currentPage}/{maxPage}");
+    }
+
+    /// <summary>
+    /// 重置頁面資料，並指定要跳到的頁數
+    /// </summary>
+    private void ResetToPage(int toPage)
+    {
+        // 計算總頁數
+        maxPage = Mathf.CeilToInt((float)currentCaches.Count / slotPerPage);
+        currentPage = Mathf.Clamp(toPage, 1, maxPage);
+
+        // 顯示頁數
+        uiCollector.SetText(UIKey.Closet_PageText, $"{currentPage}/{maxPage}");
+    }
+
+    /// <summary>
+    /// 顯示指定頁數的項目在Slot上
+    /// </summary>
+    private void DisplayBodyItems(int page)
+    {
+        foreach (ClosetSlot slot in closetSlots)
+        {
+            slot.Clear();
+        }
+
+        int startIndex = (page - 1) * slotPerPage;
+        int endIndex = Mathf.Min(startIndex + slotPerPage, currentCaches.Count);
+        int displayCount = endIndex - startIndex;
+        if (displayCount == 0)
+        {
+            return;
+        }
+
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            closetSlots[i - startIndex].Display(currentCaches[i]);
+        }
     }
 }
