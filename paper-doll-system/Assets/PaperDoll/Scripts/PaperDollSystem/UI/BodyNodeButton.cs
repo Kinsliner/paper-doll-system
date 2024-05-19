64801cf4495532f325afacc175b46cc3876b70e3
echen <echen.wan9@gmail.com>
2024-05-19 15:54:16 +0800

    [新增]部位按鈕

---
+++ paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/BodyNodeButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class BodyNodeButton : MonoBehaviour
+{
+    public Action<BodyNode> OnClickEvent;
+
+    private BodyNode bodyNode;
+
+    public void Init(BodyNode bodyNode)
+    {
+        this.bodyNode = bodyNode;
+    }
+
+    public void OnClick()
+    {
+        OnClickEvent?.Invoke(bodyNode);
+    }
+}
