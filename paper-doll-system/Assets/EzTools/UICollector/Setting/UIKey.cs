a7ac211d8fe1d5de72fdfc65585e9e8c5daa7811
echen <echen.wan9@gmail.com>
2024-05-19 18:18:33 +0800

    [更新]UIkey

--- paper-doll-system/Assets/EzTools/UICollector/Setting/UIKey.cs
+++ paper-doll-system/Assets/EzTools/UICollector/Setting/UIKey.cs
@@ -12,5 +12,21 @@ namespace Ez.Tool
         public const string Closet_PrevButton = "Closet_PrevButton";
         
         public const string Closet_PageText = "Closet_PageText";
+        
+        public const string BodyNodeButton_IconImage = "BodyNodeButton_IconImage";
+        
+        public const string ClosetSlot_Free = "ClosetSlot_Free";
+        
+        public const string ClosetSlot_Lock = "ClosetSlot_Lock";
+        
+        public const string ClosetSlot_Select = "ClosetSlot_Select";
+        
+        public const string ClosetSlot_BodyRoot = "ClosetSlot_BodyRoot";
+        
+        public const string ClosetSlot_HeadRoot = "ClosetSlot_HeadRoot";
+        
+        public const string ClosetSlot_BodyImage = "ClosetSlot_BodyImage";
+        
+        public const string ClosetSlot_HeadImage = "ClosetSlot_HeadImage";
     }
 }
