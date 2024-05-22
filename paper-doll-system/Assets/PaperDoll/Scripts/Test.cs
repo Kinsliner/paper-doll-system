e21e0a6fc7806aba50c8dc32f01a6fe5a44e4f6b
echen <echen.wan9@gmail.com>
2024-05-22 17:08:12 +0800

    [更新]測試功能

--- paper-doll-system/Assets/PaperDoll/Scripts/Test.cs
+++ paper-doll-system/Assets/PaperDoll/Scripts/Test.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public ClosetPanelUI closetPanelUI;
+    public PaperDoll character;
+    public CharacterRoot characterRoot;
 
     // Start is called before the first frame update
     void Start()
@@ -12,10 +14,16 @@ public class Test : MonoBehaviour
         ModelAssetManager.Init();
         PaperDollManager.Init();
 
-        if(closetPanelUI != null)
+        if (closetPanelUI != null)
         {
             closetPanelUI.Init();
         }
+        if (characterRoot != null)
+        {
+            characterRoot.Init();
+        }
+
+        PaperDollManager.Controller.SetupPaperDoll(character, BodyNode.Head, BodyNode.Body);
     }
 
     // Update is called once per frame
