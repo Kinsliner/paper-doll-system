f0a8ea7b7c8fc0b3426c0f8f6178ad3d5947fa65
echen <echen.wan9@gmail.com>
2024-05-26 18:31:26 +0800

    [新增]角色控制面板UI功能

---
+++ paper-doll-system/Assets/PaperDoll/Scripts/PaperDollSystem/UI/CharacterPanelUI.cs
@@ -0,0 +1,63 @@
+using Ez.Tool;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPanelUI : MonoBehaviour
+{
+    [SerializeField]
+    private UICollector uiCollector;
+
+    private PaperDollController paperDollController;
+    private BodyDirection currentDirection = BodyDirection.Front;
+    private int turnCount = 0;
+
+    // Start is called before the first frame update
+    public void Init()
+    {
+        paperDollController = PaperDollManager.Controller;
+        turnCount = (int)currentDirection;
+
+        if (uiCollector != null)
+        {
+            uiCollector.BindOnClick(UIKey.Character_TurnLeftButton, OnTurnLeftClick);
+            uiCollector.BindOnClick(UIKey.Character_TurnRightButton, OnTurnRightClick);
+        }
+    }
+
+    private void OnTurnRightClick()
+    {
+        CalculateTurnCount(1);
+
+        if (paperDollController != null)
+        {
+            paperDollController.Turn(currentDirection);
+        }
+    }
+
+    private void OnTurnLeftClick()
+    {
+        CalculateTurnCount(-1);
+
+        if (paperDollController != null)
+        {
+            paperDollController.Turn(currentDirection);
+        }
+    }
+
+    private void CalculateTurnCount(int offset)
+    {
+        turnCount += offset;
+        if (turnCount < 0)
+        {
+            turnCount = 3;
+        }
+        else if (turnCount > 3)
+        {
+            turnCount = 0;
+        }
+
+        currentDirection = (BodyDirection)turnCount;
+    }
+}
