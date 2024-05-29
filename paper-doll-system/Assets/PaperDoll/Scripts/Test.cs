461db39768761e2433a42463c5ea15f1b86114c5
echen <echen.wan9@gmail.com>
2024-05-29 21:19:18 +0800

    [更新]測試用功能

--- paper-doll-system/Assets/PaperDoll/Scripts/Test.cs
+++ paper-doll-system/Assets/PaperDoll/Scripts/Test.cs
@@ -1,60 +1,37 @@
 using Ez.Input;
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public ClosetPanelUI closetPanelUI;
-    public CharacterPanelUI characterPanelUI;
     public PaperDoll character;
-    public CharacterRoot characterRoot;
     public int testLcokID = 1;
     public float moveSpeed = 5.0f;
+
+    private PaperDollSystem paperDollSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        ModelAssetManager.Init();
-        PaperDollManager.Init();
-
-        if (closetPanelUI != null)
-        {
-            closetPanelUI.Init();
-        }
-        if (characterPanelUI != null)
-        {
-            characterPanelUI.Init();
-        }
-        if (characterRoot != null)
-        {
-            characterRoot.Init();
-        }
-
-        PaperDollManager.Controller.SetupPaperDoll(character, BodyNode.Head, BodyNode.Body);
-
-        var movement = new Movement();
-        movement.SetTarget(character.transform);
-        movement.SetPaperDoll(character);
-        movement.Init();
-        movement.Speed = moveSpeed;
-        InputSystem.Register(new KeyBoardMoveInput(), movement);
+        PaperDollSystem.ActivePanel();
+        PaperDollSystem.SetPaperDoll(character);
     }
 
     private void Update()
     {
-        InputSystem.Update();
+        
     }
 
     [ContextMenu("Lock")]
     private void Lock()
     {
-        PaperDollManager.Controller.Lock(testLcokID);
+        PaperDollSystem.Lock(testLcokID);
     }
 
     [ContextMenu("UnLock")]
     private void UnLock()
     {
-        PaperDollManager.Controller.Unlock(testLcokID);
+        PaperDollSystem.Unlock(testLcokID);
     }
 }
