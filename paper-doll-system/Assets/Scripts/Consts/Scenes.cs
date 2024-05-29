d7f2ed6c31202de43e1e0345bc39df1fe495b625
echen <echen.wan9@gmail.com>
2024-05-29 21:21:49 +0800

    [新增]場景常數表

---
+++ paper-doll-system/Assets/Scripts/Consts/Scenes.cs
@@ -0,0 +1,10 @@
+
+namespace Ez.Tool
+{
+    public class Scenes
+    {
+        public const string Scenes_Example = "Scenes/Example";
+        
+        public const string Scenes_PaperDollSystem = "Scenes/PaperDollSystem";
+    }
+}
